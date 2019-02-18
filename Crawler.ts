845ee3faf280621b7b0d5468839f7630d3c33515
Geusan Lim <dnay2k@gmail.com>
2019-02-19 01:40:17 +0900

    add methods

--- Crawler.ts
+++ Crawler.ts
@@ -11,4 +11,26 @@ export class Crawler {
     this.baseUrl = baseUrl;
   }
 
+  /**
+   * 엔드포인트를 입력해서 baseUrl의 세부 페이지를 로드
+   * @param target endpoint
+   */
+  public async load(target: string) {
+    // load context
+  }
+
+  /**
+   * 다음 페이지 이동
+   */
+  public async next() {
+    // next context
+  }
+
+  /**
+   * 현재 페이지 크롤링
+   */
+  public async crawl() {
+    // crawl context
+  }
+
 }
