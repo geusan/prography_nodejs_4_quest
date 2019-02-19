26817fcb3ee8f9cfaf3360ecec3aa3251223f821
Geusan Lim <dnay2k@gmail.com>
2019-02-19 22:43:13 +0900

    Crawler 개편..

---
+++ src/Crawler.ts
@@ -0,0 +1,106 @@
+import axios from 'axios';
+import jsdom, {JSDOM} from 'jsdom';
+import signale from 'signale';
+import { SeleniumDriver } from './SeleniumDriver';
+
+// 책 정보 구조 정의
+export interface IBook {
+  imgUrl: string;
+  price: string;
+  title: string;
+  score: string;
+}
+
+export class Crawler {
+  private baseUrl: string;
+  private documentLoader: SeleniumDriver;
+  private currentDom: JSDOM;
+  private endpoint = '';
+  private currentPage = 1;
+
+  // private readonly encode: Iconv.Iconv = new Iconv('euc-kr', 'utf-8');
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+    this.documentLoader = new SeleniumDriver();
+  }
+
+  /**
+   * 엔드포인트를 입력해서 baseUrl의 세부 페이지를 로드
+   * @param target endpoint
+   */
+  public async load(target: string, params: { [key: string]: any }) {
+    this.endpoint = target;
+    // load context
+    const html = await this.get(target, params);
+    return this;
+  }
+
+  /**
+   * 다음 페이지 이동
+   */
+  public async next() {
+    // next context
+    const c: any = this.currentDom.window;
+    await this.get(this.endpoint, {
+      vPplace: 'top',
+      vPstartno: this.currentPage * 20,
+      vPstrCategory: 'TOT',
+      vPstrKeyWord: 'node.js',
+    });
+    this.currentPage += 1;
+
+    return this;
+  }
+
+  /**
+   * 현재 페이지 크롤링
+   */
+  public async crawl(dom?: JSDOM): Promise<IBook[]> {
+    // crawl context
+    if (!dom && !this.currentDom) { return; }
+    const body = (dom || this.currentDom).window.document.body;
+    const rows = body.querySelectorAll(`#container > div:nth-child(19) > form > table tr`);
+    const results: IBook[] = [];
+    rows.forEach((node) => {
+      // 이미지 가져오기
+      let imgUrl;
+      if (node.querySelector('.cover img')) {
+        imgUrl = node.querySelector('.cover img').getAttribute('src');
+      }
+      // 제목 가져오기
+      let title;
+      if (node.querySelector('.title a')) {
+        title = node.querySelector('.title a').textContent
+          .replace(/\s{1,}/g, ' ').replace(/,/g, '').trim();
+        // signale.log(encoding, title);
+      }
+      // 평점 가져오기
+      let score;
+      if (node.querySelector('.review.klover a')) {
+        score = node.querySelector('.review.klover a').textContent
+          .replace('Klover', '').replace(/\s{1,}/g, ' ').replace(/,/g, '').trim();
+      }
+      // 가격 가져오기
+      let price;
+      if (node.querySelector('.org_price del')) {
+        price = node.querySelector('.org_price del').textContent
+          .replace(/\s{1,}/g, ' ').replace(/,/g, '').trim();
+      }
+      results.push({ imgUrl, title, price, score });
+    });
+
+    return results;
+  }
+
+  public async get(url: string, params: { [key: string]: any }) {
+    const html = await this.documentLoader.get(this.baseUrl + url, params);
+    this.currentDom = new JSDOM(html);
+    return html;
+  }
+
+  public quit() {
+    this.documentLoader.quit();
+  }
+
+}
